820159e596ba753b3c6952113c2c20cb2a0af271
maciic <focimaci99@gmail.com>
2021-12-06 18:24:00 +0100

    feat: create menu bar, fragments, video bar

--- MasodikBead/app/src/main/java/com/example/masodikbead/MainActivity.kt
+++ MasodikBead/app/src/main/java/com/example/masodikbead/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.masodikbead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.first_Fragment_home, R.id.second_Fragment_video, R.id.third_Fragment_text))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
